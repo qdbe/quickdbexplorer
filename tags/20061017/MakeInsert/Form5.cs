3f2bb79aadecc4ed14b5b8a1fc29948080d05420
godz@systemd.co.jp <godz@systemd.co.jp@1a8f6471-1045-0410-b12e-b7bf80dad2f1>
2006-10-17 15:18:24 +0000

    リモート操作によるコピー

---
+++ tags/20061017/MakeInsert/Form5.cs
@@ -0,0 +1,226 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace MakeInsert
+{
+	/// <summary>
+	/// Form5 の概要の説明です。
+	/// </summary>
+	public class Form5 : System.Windows.Forms.Form
+	{
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+
+		public string	SelectSql = "";
+		public bool		hasReturn = false;
+		private System.Windows.Forms.ContextMenu contextMenu1;
+		private System.Windows.Forms.MenuItem menuItem1;
+		private System.Windows.Forms.MenuItem menuItem2;
+		private System.Windows.Forms.MenuItem menuItem3;
+		private System.Windows.Forms.MenuItem menuItem4;
+		private System.Windows.Forms.CheckBox checkBox1;
+		/// <summary>
+		/// 必要なデザイナ変数です。
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		public Form5()
+		{
+			//
+			// Windows フォーム デザイナ サポートに必要です。
+			//
+			InitializeComponent();
+
+			//
+			// TODO: InitializeComponent 呼び出しの後に、コンストラクタ コードを追加してください。
+			//
+		}
+
+		/// <summary>
+		/// 使用されているリソースに後処理を実行します。
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows フォーム デザイナで生成されたコード 
+		/// <summary>
+		/// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディタで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.contextMenu1 = new System.Windows.Forms.ContextMenu();
+			this.menuItem1 = new System.Windows.Forms.MenuItem();
+			this.menuItem2 = new System.Windows.Forms.MenuItem();
+			this.menuItem3 = new System.Windows.Forms.MenuItem();
+			this.menuItem4 = new System.Windows.Forms.MenuItem();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.AcceptsReturn = true;
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+				| System.Windows.Forms.AnchorStyles.Left) 
+				| System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.ContextMenu = this.contextMenu1;
+			this.textBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+			this.textBox1.Location = new System.Drawing.Point(16, 24);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox1.Size = new System.Drawing.Size(444, 232);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "";
+			this.textBox1.WordWrap = false;
+			// 
+			// contextMenu1
+			// 
+			this.contextMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+																						 this.menuItem1,
+																						 this.menuItem2,
+																						 this.menuItem3,
+																						 this.menuItem4});
+			// 
+			// menuItem1
+			// 
+			this.menuItem1.Index = 0;
+			this.menuItem1.Shortcut = System.Windows.Forms.Shortcut.CtrlC;
+			this.menuItem1.Text = "コピー";
+			this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+			// 
+			// menuItem2
+			// 
+			this.menuItem2.Index = 1;
+			this.menuItem2.Shortcut = System.Windows.Forms.Shortcut.CtrlX;
+			this.menuItem2.Text = "切り取り";
+			this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+			// 
+			// menuItem3
+			// 
+			this.menuItem3.Index = 2;
+			this.menuItem3.Shortcut = System.Windows.Forms.Shortcut.CtrlV;
+			this.menuItem3.Text = "貼り付け";
+			this.menuItem3.Click += new System.EventHandler(this.menuItem3_Click);
+			// 
+			// menuItem4
+			// 
+			this.menuItem4.Index = 3;
+			this.menuItem4.Shortcut = System.Windows.Forms.Shortcut.CtrlA;
+			this.menuItem4.Text = "全て選択";
+			this.menuItem4.Click += new System.EventHandler(this.menuItem4_Click);
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.button1.Location = new System.Drawing.Point(16, 292);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(96, 24);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "SQL実行";
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.button2.Location = new System.Drawing.Point(348, 292);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(88, 24);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "キャンセル";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.checkBox1.Location = new System.Drawing.Point(40, 272);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(208, 16);
+			this.checkBox1.TabIndex = 3;
+			this.checkBox1.Text = "戻り値あり";
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// Form5
+			// 
+			this.AcceptButton = this.button1;
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 12);
+			this.CancelButton = this.button2;
+			this.ClientSize = new System.Drawing.Size(480, 325);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form5";
+			this.ShowInTaskbar = false;
+			this.Text = "SQL文指定";
+			this.Load += new System.EventHandler(this.Form5_Load);
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			SelectSql = this.textBox1.Text;
+			if( this.checkBox1.Checked == true )
+			{
+				this.hasReturn = true;
+			}
+			else
+			{
+				this.hasReturn = false;
+			}
+			//this.DialogResult = DialogResult.OK;
+		}
+
+		private void Form5_Load(object sender, System.EventArgs e)
+		{
+			this.textBox1.Text = SelectSql;
+			this.textBox1.Focus();
+		}
+
+		private void menuItem1_Click(object sender, System.EventArgs e)
+		{
+			this.textBox1.Copy();
+		}
+
+		private void menuItem2_Click(object sender, System.EventArgs e)
+		{
+			this.textBox1.Cut();
+		}
+
+		private void menuItem3_Click(object sender, System.EventArgs e)
+		{
+			this.textBox1.Paste();
+		}
+
+		private void menuItem4_Click(object sender, System.EventArgs e)
+		{
+			this.textBox1.SelectAll();
+		}
+
+		private void checkBox1_CheckedChanged(object sender, System.EventArgs e)
+		{
+		
+		}
+
+	}
+}
---
+++ tags/20061017/MakeInsert/saveClass.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Xml;
+
+namespace MakeInsert
+{
+	[Serializable]
+	public class ServerData
+	{
+		protected string		servername;
+		protected string		instancename;
+		protected string		keyname;
+
+		public string		lastdb;		// 最後に利用したDB名
+
+		public Hashtable		dbopt;		// DB 毎の最終ユーザーを記録する
+		public int	isShowsysuser;		// システムユーザーを表示するか否か
+		public int	sortKey;
+		public int	showView;
+		public	Hashtable	outdest;
+		public	Hashtable	outfile;
+		public	Hashtable	showgrid;
+		public 	Hashtable	griddspcnt;
+		public	Hashtable	txtencode;
+		public	bool	isSaveKey = true;
+		public	bool	IsUseTrust = false;
+
+		public ServerData()
+		{
+			dbopt = new Hashtable();
+			isShowsysuser = 0;
+			sortKey = 0;
+			showView = 0;
+			IsUseTrust = false;
+			outdest =  new Hashtable();
+			outfile =  new Hashtable();
+			showgrid =  new Hashtable();
+			griddspcnt =  new Hashtable();
+			txtencode = new Hashtable();
+		}
+
+		public string Servername
+		{
+			get { return this.servername; }
+			set 
+			{ 
+				this.servername = value; 
+				this.keyname = value + this.instancename;
+			}
+		}
+		public string InstanceName
+		{
+			get { return this.instancename; }
+			set 
+			{
+				this.instancename = value;
+				this.keyname = this.servername + this.instancename;
+			}
+		}
+		public string KeyName
+		{
+			get 
+			{ 
+				return this.keyname; 
+			}
+		}
+	}
+
+	/// <summary>
+	/// saveClass の概要の説明です。
+	/// </summary>
+	[Serializable]
+	public class saveClass
+	{
+		public Hashtable	ht;
+		public saveClass()
+		{
+			ht = new Hashtable();
+		}
+	}
+}
