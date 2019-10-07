55444cc90efec115724636beaaece6bd7b6e8252
godz.qdbe@gmail.com <godz.qdbe@gmail.com>
2019-10-07 20:48:00 +0900

    refs #53

--- quickDBExplorer/Forms/Dialog/WhereDialog.cs
+++ quickDBExplorer/Forms/Dialog/WhereDialog.cs
@@ -16,10 +16,18 @@ namespace quickDBExplorer
 		private System.ComponentModel.IContainer components = null;
 
 		private const string BASETABLE = "Base";
-		private const string FIELDNAME = "Fields";
-		private const string CONDCMB = "cond";
+
+        private const string CLEARBUTTON = "ClearButton";
+        private const string FIELDNAME = "Fields";
+        private const string CONDCMB = "cond";
 		private const string VALUES = "datavalues";
 		private const string FIELDTYPE = "fieldtypes";
+
+        private const int COL_CLEAR_BUTTON = 0;
+        private const int COL_LABEL = 1;
+        private const int COL_HIDDEN_FIELDTYPE = 2;
+        private const int COL_COND = 3;
+        private const int COL_VAL = 4;
 
 		private DataSet fieldDs = null;
 		private DataGridView fieldCondition;
@@ -33,10 +41,6 @@ namespace quickDBExplorer
 		/// </summary>
 		private DBObjectInfo pTargetObject;
 		private DBObjectInfo preTartgetObject = null;
-		private DataGridViewTextBoxColumn Fields;
-		private DataGridViewTextBoxColumn fieldtypes;
-		private DataGridViewComboBoxColumn cond;
-		private DataGridViewTextBoxColumn datavalues;
 		
 		/// <summary>
 		/// where 句指定の対象オブジェクト
@@ -68,11 +72,16 @@ namespace quickDBExplorer
 		/// オブジェクトの alias
 		/// </summary>
 		private string preAliasName = "";
+        private DataGridViewButtonColumn clearLine;
+        private DataGridViewTextBoxColumn Fields;
+        private DataGridViewTextBoxColumn fieldtypes;
+        private DataGridViewComboBoxColumn cond;
+        private DataGridViewTextBoxColumn datavalues;
 
-		/// <summary>
-		/// オブジェクトの alias
-		/// </summary>
-		private string pAliasName = "";
+        /// <summary>
+        /// オブジェクトの alias
+        /// </summary>
+        private string pAliasName = "";
 
 		/// <summary>
 		/// オブジェクトの alias
@@ -123,100 +132,119 @@ namespace quickDBExplorer
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.fieldCondition = new System.Windows.Forms.DataGridView();
-			this.Fields = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.fieldtypes = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.cond = new System.Windows.Forms.DataGridViewComboBoxColumn();
-			this.datavalues = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.btnClear = new System.Windows.Forms.Button();
-			this.cmbCondition = new System.Windows.Forms.ComboBox();
-			((System.ComponentModel.ISupportInitialize)(this.fieldCondition)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// chkStayOnTop
-			// 
-			this.chkStayOnTop.TabIndex = 8;
-			// 
-			// btnApply
-			// 
-			this.btnApply.Location = new System.Drawing.Point(462, 362);
-			this.btnApply.TabIndex = 6;
-			// 
-			// txtZoom
-			// 
-			this.txtZoom.AcceptsReturn = true;
-			this.txtZoom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.txtZoom.Size = new System.Drawing.Size(624, 114);
-			// 
-			// btnOk
-			// 
-			this.btnOk.Location = new System.Drawing.Point(14, 362);
-			this.btnOk.TabIndex = 5;
-			// 
-			// btnClose
-			// 
-			this.btnClose.Location = new System.Drawing.Point(558, 362);
-			this.btnClose.TabIndex = 7;
-			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-			// 
-			// MsgArea
-			// 
-			this.MsgArea.Location = new System.Drawing.Point(106, 362);
-			this.MsgArea.Size = new System.Drawing.Size(350, 26);
-			// 
-			// fieldCondition
-			// 
-			this.fieldCondition.AllowUserToAddRows = false;
-			this.fieldCondition.AllowUserToDeleteRows = false;
-			this.fieldCondition.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-						| System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.fieldCondition.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
-			this.fieldCondition.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-			this.fieldCondition.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.fieldCondition.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.fieldCondition = new System.Windows.Forms.DataGridView();
+            this.clearLine = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Fields = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fieldtypes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cond = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.datavalues = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.cmbCondition = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fieldCondition)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chkStayOnTop
+            // 
+            this.chkStayOnTop.TabIndex = 8;
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(462, 362);
+            this.btnApply.TabIndex = 6;
+            // 
+            // txtZoom
+            // 
+            this.txtZoom.AcceptsReturn = true;
+            this.txtZoom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtZoom.Size = new System.Drawing.Size(624, 114);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(14, 362);
+            this.btnOk.TabIndex = 5;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(558, 362);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // MsgArea
+            // 
+            this.MsgArea.Location = new System.Drawing.Point(106, 362);
+            this.MsgArea.Size = new System.Drawing.Size(350, 26);
+            // 
+            // fieldCondition
+            // 
+            this.fieldCondition.AllowUserToAddRows = false;
+            this.fieldCondition.AllowUserToDeleteRows = false;
+            this.fieldCondition.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fieldCondition.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.fieldCondition.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.fieldCondition.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.fieldCondition.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clearLine,
             this.Fields,
             this.fieldtypes,
             this.cond,
             this.datavalues});
-			this.fieldCondition.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
-			this.fieldCondition.Location = new System.Drawing.Point(14, 169);
-			this.fieldCondition.Name = "fieldCondition";
-			this.fieldCondition.RowHeadersVisible = false;
-			this.fieldCondition.RowTemplate.Height = 21;
-			this.fieldCondition.Size = new System.Drawing.Size(622, 160);
-			this.fieldCondition.TabIndex = 3;
-			this.fieldCondition.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.fieldCondition_CellLeave);
-			this.fieldCondition.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.fieldCondition_CellEndEdit);
-			this.fieldCondition.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.fieldCondition_CellEnter);
-			// 
-			// Fields
-			// 
-			this.Fields.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-			this.Fields.DataPropertyName = "Fields";
-			this.Fields.HeaderText = "フィールド名";
-			this.Fields.Name = "Fields";
-			this.Fields.ReadOnly = true;
-			this.Fields.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-			this.Fields.Width = 42;
-			// 
-			// fieldtypes
-			// 
-			this.fieldtypes.DataPropertyName = "fieldtypes";
-			this.fieldtypes.HeaderText = "";
-			this.fieldtypes.Name = "fieldtypes";
-			this.fieldtypes.ReadOnly = true;
-			this.fieldtypes.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-			this.fieldtypes.Visible = false;
-			this.fieldtypes.Width = 5;
-			// 
-			// cond
-			// 
-			this.cond.DataPropertyName = "cond";
-			this.cond.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.ComboBox;
-			this.cond.HeaderText = "結合条件指定";
-			this.cond.Items.AddRange(new object[] {
+            this.fieldCondition.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.fieldCondition.Location = new System.Drawing.Point(14, 169);
+            this.fieldCondition.Name = "fieldCondition";
+            this.fieldCondition.RowHeadersVisible = false;
+            this.fieldCondition.RowTemplate.Height = 21;
+            this.fieldCondition.Size = new System.Drawing.Size(622, 160);
+            this.fieldCondition.TabIndex = 3;
+            this.fieldCondition.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.FieldCondition_CellContentClick);
+            this.fieldCondition.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.fieldCondition_CellEndEdit);
+            this.fieldCondition.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.fieldCondition_CellEnter);
+            this.fieldCondition.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.fieldCondition_CellLeave);
+            // 
+            // clearLine
+            // 
+            this.clearLine.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.clearLine.DataPropertyName = "ClearButton";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.clearLine.DefaultCellStyle = dataGridViewCellStyle1;
+            this.clearLine.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.clearLine.HeaderText = "クリア";
+            this.clearLine.Name = "clearLine";
+            this.clearLine.Text = "クリア";
+            this.clearLine.ToolTipText = "選択行をクリア";
+            this.clearLine.UseColumnTextForButtonValue = true;
+            this.clearLine.Width = 45;
+            // 
+            // Fields
+            // 
+            this.Fields.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Fields.DataPropertyName = "Fields";
+            this.Fields.HeaderText = "フィールド名";
+            this.Fields.Name = "Fields";
+            this.Fields.ReadOnly = true;
+            this.Fields.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Fields.Width = 50;
+            // 
+            // fieldtypes
+            // 
+            this.fieldtypes.DataPropertyName = "fieldtypes";
+            this.fieldtypes.HeaderText = "";
+            this.fieldtypes.Name = "fieldtypes";
+            this.fieldtypes.ReadOnly = true;
+            this.fieldtypes.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.fieldtypes.Visible = false;
+            this.fieldtypes.Width = 5;
+            // 
+            // cond
+            // 
+            this.cond.DataPropertyName = "cond";
+            this.cond.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.ComboBox;
+            this.cond.HeaderText = "結合条件指定";
+            this.cond.Items.AddRange(new object[] {
             "",
             "=",
             "!=",
@@ -229,67 +257,66 @@ namespace quickDBExplorer
             "<",
             ">",
             ">=",
-            "<=",
-			});
-			this.cond.MinimumWidth = 50;
-			this.cond.Name = "cond";
-			this.cond.Width = 53;
-			// 
-			// datavalues
-			// 
-			this.datavalues.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-			this.datavalues.DataPropertyName = "datavalues";
-			this.datavalues.HeaderText = "値";
-			this.datavalues.MinimumWidth = 100;
-			this.datavalues.Name = "datavalues";
-			this.datavalues.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-			// 
-			// btnClear
-			// 
-			this.btnClear.Location = new System.Drawing.Point(14, 335);
-			this.btnClear.Name = "btnClear";
-			this.btnClear.Size = new System.Drawing.Size(181, 23);
-			this.btnClear.TabIndex = 4;
-			this.btnClear.Text = "フィールド条件クリア";
-			this.btnClear.UseVisualStyleBackColor = true;
-			this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
-			// 
-			// cmbCondition
-			// 
-			this.cmbCondition.FormattingEnabled = true;
-			this.cmbCondition.Items.AddRange(new object[] {
+            "<="});
+            this.cond.MinimumWidth = 50;
+            this.cond.Name = "cond";
+            this.cond.Width = 53;
+            // 
+            // datavalues
+            // 
+            this.datavalues.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.datavalues.DataPropertyName = "datavalues";
+            this.datavalues.HeaderText = "値";
+            this.datavalues.MinimumWidth = 100;
+            this.datavalues.Name = "datavalues";
+            this.datavalues.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(14, 335);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(181, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "フィールド条件を全てクリア";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // cmbCondition
+            // 
+            this.cmbCondition.FormattingEnabled = true;
+            this.cmbCondition.Items.AddRange(new object[] {
             "AND",
             "OR"});
-			this.cmbCondition.Location = new System.Drawing.Point(14, 144);
-			this.cmbCondition.Name = "cmbCondition";
-			this.cmbCondition.Size = new System.Drawing.Size(154, 20);
-			this.cmbCondition.TabIndex = 2;
-			this.cmbCondition.SelectedIndexChanged += new System.EventHandler(this.cmbCondition_SelectedIndexChanged);
-			this.cmbCondition.TextChanged += new System.EventHandler(this.cmbCondition_TextChanged);
-			// 
-			// WhereDialog
-			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5, 12);
-			this.ClientSize = new System.Drawing.Size(648, 394);
-			this.Controls.Add(this.fieldCondition);
-			this.Controls.Add(this.cmbCondition);
-			this.Controls.Add(this.btnClear);
-			this.Name = "WhereDialog";
-			this.Text = "Where 句指定";
-			this.Load += new System.EventHandler(this.WhereDialog_Load);
-			this.VisibleChanged += new System.EventHandler(this.WhereDialog_VisibleChanged);
-			this.Controls.SetChildIndex(this.chkStayOnTop, 0);
-			this.Controls.SetChildIndex(this.btnClear, 0);
-			this.Controls.SetChildIndex(this.btnOk, 0);
-			this.Controls.SetChildIndex(this.btnClose, 0);
-			this.Controls.SetChildIndex(this.btnApply, 0);
-			this.Controls.SetChildIndex(this.cmbCondition, 0);
-			this.Controls.SetChildIndex(this.MsgArea, 0);
-			this.Controls.SetChildIndex(this.fieldCondition, 0);
-			this.Controls.SetChildIndex(this.txtZoom, 0);
-			((System.ComponentModel.ISupportInitialize)(this.fieldCondition)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.cmbCondition.Location = new System.Drawing.Point(14, 144);
+            this.cmbCondition.Name = "cmbCondition";
+            this.cmbCondition.Size = new System.Drawing.Size(154, 20);
+            this.cmbCondition.TabIndex = 2;
+            this.cmbCondition.SelectedIndexChanged += new System.EventHandler(this.cmbCondition_SelectedIndexChanged);
+            this.cmbCondition.TextChanged += new System.EventHandler(this.cmbCondition_TextChanged);
+            // 
+            // WhereDialog
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 12);
+            this.ClientSize = new System.Drawing.Size(648, 394);
+            this.Controls.Add(this.fieldCondition);
+            this.Controls.Add(this.cmbCondition);
+            this.Controls.Add(this.btnClear);
+            this.Name = "WhereDialog";
+            this.Text = "Where 句指定";
+            this.Load += new System.EventHandler(this.WhereDialog_Load);
+            this.VisibleChanged += new System.EventHandler(this.WhereDialog_VisibleChanged);
+            this.Controls.SetChildIndex(this.chkStayOnTop, 0);
+            this.Controls.SetChildIndex(this.btnClear, 0);
+            this.Controls.SetChildIndex(this.btnOk, 0);
+            this.Controls.SetChildIndex(this.btnClose, 0);
+            this.Controls.SetChildIndex(this.btnApply, 0);
+            this.Controls.SetChildIndex(this.cmbCondition, 0);
+            this.Controls.SetChildIndex(this.MsgArea, 0);
+            this.Controls.SetChildIndex(this.fieldCondition, 0);
+            this.Controls.SetChildIndex(this.txtZoom, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.fieldCondition)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 		#endregion
@@ -321,7 +348,8 @@ namespace quickDBExplorer
 			// 表示するデータを生成する
 			this.fieldDs = new DataSet();
 			fieldDs.Tables.Add(BASETABLE);
-			fieldDs.Tables[BASETABLE].Columns.Add(FIELDNAME);
+            fieldDs.Tables[BASETABLE].Columns.Add(CLEARBUTTON);
+            fieldDs.Tables[BASETABLE].Columns.Add(FIELDNAME);
 			fieldDs.Tables[BASETABLE].Columns.Add(FIELDTYPE);
 			fieldDs.Tables[BASETABLE].Columns.Add(CONDCMB);
 			fieldDs.Tables[BASETABLE].Columns.Add(VALUES);
@@ -343,7 +371,7 @@ namespace quickDBExplorer
 
 		private void fieldCondition_CellEndEdit(object sender, DataGridViewCellEventArgs e)
 		{
-			if (e.ColumnIndex == 3 &&
+			if (e.ColumnIndex == COL_VAL &&
 				this.fieldCondition[e.ColumnIndex, e.RowIndex].Value != null &&
 				!this.fieldCondition[e.ColumnIndex, e.RowIndex].Value.Equals(string.Empty) &&
 				this.fieldCondition[e.ColumnIndex, e.RowIndex].Value != DBNull.Value)
@@ -528,7 +556,7 @@ namespace quickDBExplorer
 		}
 
 		/// <summary>
-		/// 条件をクリアする
+		/// 全ての条件をクリアする
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
@@ -609,6 +637,17 @@ namespace quickDBExplorer
 				}
 			}
 		}
-	}
+
+        private void FieldCondition_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == COL_CLEAR_BUTTON)
+            {
+                this.fieldCondition[COL_COND, e.RowIndex].Value = "";
+                this.fieldCondition[COL_VAL, e.RowIndex].Value = "";
+            }
+            SetFieldCondResult();
+
+        }
+    }
 }
 
