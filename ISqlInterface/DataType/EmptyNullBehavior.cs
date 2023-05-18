85315355ee44e2e9bd49ca09f6aab7d2742fb00a
godz.qdbe@gmail.com <godz.qdbe@gmail.com>
2023-05-18 12:58:54 +0900

    refs #78 .NET 4 support

---
+++ ISqlInterface/DataType/EmptyNullBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace quickDBExplorer.DataType
+{
+    /// <summary>
+    /// 空文字とNULLの扱い
+    /// </summary>
+    public enum EmptyNullBehavior
+    {
+        /// <summary>
+        /// 変換なし
+        /// </summary>
+        NoConv = 0,
+        /// <summary>
+        /// NULLとして設定
+        /// </summary>
+        AsNULL = 1,
+        /// <summary>
+        /// 空文字として設定
+        /// </summary>
+        AsEmpty = 2
+    }
+}
